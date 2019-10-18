553c47d8fd0b9a64c6de112d4b4666fcef09d6e3
luciano <luciano-menezes2010@hotmail.com>
2019-10-18 14:23:36 -0300

    Commit inicial

---
+++ src/pages/login/index.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+
+export default function Login({ history }) {
+
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/session', { email });
+
+    const { _id } = response.data.user;
+
+    localStorage.setItem('user', _id);
+
+    history.push('/dashboard');
+  }
+
+  return (
+    <>
+      <p>
+        Ofereça <strong>spot</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+  </p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-MAIL*</label>
+        <input
+          className="email"
+          id="email"
+          type="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+          placeholder="Digite seu melhor e-mail" />
+        <button className="btn" type="submit">Entrar</button>
+      </form>
+    </>
+  )
+}
